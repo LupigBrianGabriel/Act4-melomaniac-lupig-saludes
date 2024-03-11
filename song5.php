8bcb6391ae4df511ae0dad59627e109bb6d3377e
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:05:40 +0800

    song5 commited by lupig

---
+++ song5.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Home</h2>
+    <h4>Artist: Luke Chiang</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "I've been away <br>";
+      echo "For so many days, I'm missing it all <br>";
+      echo "Where did our time go <br>";
+      echo "I'm scrollin' through pictures saved on my phone <br>";
+      echo "I wanna go home <br>";
+      echo "<br>";
+      echo "And I don't know why, but I'm so, sentimental <br>";
+      echo "<br>";
+      echo "Lately I've been thinking of our memories <br>";
+      echo "All the laughs we shared <br>";
+      echo "And all the tears we cried <br>";
+      echo "<br>";
+      echo "Hope you know that I'm forever grateful <br>";
+      echo "All the time you shared <br>";
+      echo "Staying by my side <br>";
+      echo "Thank you for the love <br>";
+      echo "<br>";
+      echo "I gotta say <br>";
+      echo "There's still many days when I feel alone <br>";
+      echo "So I pray to God <br>";
+      echo "And then He reminds me, I'm not alone <br>";
+      echo "He gave me you <br>";
+      echo "<br>";
+      echo "Woah it's, just so easy to be sentimental <br>";
+      echo "<br>";
+      echo "Lately I've been thinking of our memories <br>";
+      echo "All the laughs we shared <br>";
+      echo "And all the tears we cried <br>";
+      echo "<br>";
+      echo "Hope you know that I'm forever grateful <br>";
+      echo "All the time you shared <br>";
+      echo "Staying by my side <br>";
+      echo "Thank you for the love <br>";
+      echo "<br>";
+      echo "And when I find myself in pieces on the ground <br>";
+      echo "You pick me up and turn me back around <br>";
+      echo "And any day could be our last <br>";
+      echo "So I'll be holding onto all the love we found <br>";
+      echo "<br>";
+      echo "Lately I've been thinking of our memories <br>";
+      echo "All the laughs we shared <br>";
+      echo "And all the tears we cried <br>";
+      echo "<br>";
+      echo "Hope you know that I'm forever grateful <br>";
+      echo "All the time you shared <br>";
+      echo "Staying by my side <br>";
+      echo "Thank you for the love <br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
