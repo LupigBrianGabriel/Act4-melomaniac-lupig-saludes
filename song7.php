2e6cf1354ea3e476fd57891de48571099478fb58
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:06:48 +0800

    song7 commited by lupig

---
+++ song7.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seasons</title>
+</head>
+<body>
+    <h2>Seasons</h2>
+    <h4>Artist: Wave To Earth</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "I can't be your love <br>";
+      echo "Look, it's too trivial for you now <br>";
+      echo "Oh my life is fallin' apart <br>";
+      echo "Maybe no one will notice if I disappear <br>";
+      echo "<br>";
+      echo "But I'll pray for you all the time <br>";
+      echo "If I could be by your side <br>";
+      echo "I'll give you all my life, my seasons <br>";
+      echo "<br>";
+      echo "I can't be your love <br>";
+      echo "Cause I'm afraid I'll ruin your life <br>";
+      echo "While the leaves withered away <br>";
+      echo "And grew again <br>";
+      echo "You have gone far away <br>";
+      echo "I'll be pushing up daisies <br>";
+      echo "And bring all the chances to here <br>";
+      echo "<br>";
+      echo "But I'll pray for you all the time <br>";
+      echo "If I could be by your side <br>";
+      echo "I'll give you all my life, my seasons <br>";
+      echo "By your side, I'll be your seasons <br>";
+      echo "<br>";
+      echo "My love <br>";
+      echo "I can't be your love <br>";
+      echo "Look, it's too trivial for you now <br>";
+      echo "Oh my life is fallin' apart <br>";
+      echo "Maybe no one will notice if I disappear <br>";
+      echo "<br>";
+      echo "But I'll pray for you all the time <br>";
+      echo "If I could be by your side <br>";
+      echo "I'll give you all my life, my seasons <br>";
+      echo "<br>";
+      echo "I can't be your love <br>";
+      echo "Cause I'm afraid I'll ruin your life <br>";
+      echo "While the leaves withered away <br>";
+      echo "And grew again <br>";
+      echo "You have gone far away <br>";
+      echo "I'll be pushing up daisies <br>";
+      echo "And bring all the chances to here <br>";
+      echo "<br>";
+      echo "But I'll pray for you all the time <br>";
+      echo "If I could be by your side <br>";
+      echo "I'll give you all my life, my seasons <br>";
+      echo "By your side, I'll be your seasons <br>";
+      echo "<br>";
+      echo "My love <br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
