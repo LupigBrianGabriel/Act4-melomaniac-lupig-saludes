3aaf1a5f7695a803984ce1b75df3dfbbaf387154
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:02:18 +0800

    song1 commited by lupig

---
+++ song1.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shouldn't Be</title>
+</head>
+<body>
+    <h2>Shouldn't Be</h2>
+    <h4>Artist: Luke Chiang</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "I can't stop feeling like none of this matters at <br>";
+      echo "Fool around, tell me words <br>";
+      echo "That you don't mean <br>";
+      echo "I'm already numb to it <br>";
+      echo "I'm already numb to it <br>";
+      echo "Feed me lies, tear me up, break me down <br>";
+      echo "I don't wanna be playin' these games <br>";
+      echo "<br>";
+      echo "I'm already used to it <br>";
+      echo "I'm already used to you being this cold <br>";
+      echo "<br>";
+      echo "But keepin' you close shouldn't be hard <br>";
+      echo "If you were honest when you said you missed me <br>";
+      echo "You've played with my pride <br>";
+      echo "Making me feel like we had something real <br>";
+      echo "<br>";
+      echo "I know we've been over this, it's nothing new <br>";
+      echo "You're still gonna be leaving me here <br>";
+      echo "It's easier hating you than missing you <br>";
+      echo "But I don't wanna be feelin' this way <br>";
+      echo "<br>";
+      echo "I'm already through with it <br>";
+      echo "I'm already tired of thinking at all <br>";
+      echo "<br>";
+      echo "Cause keepin' you close shouldn't be hard <br>";
+      echo "If you were honest when you said you missed me <br>";
+      echo "You've played with my pride <br>";
+      echo "Making me feel like we had something real <br>";
+      echo "And making you stay shouldn't feel wrong <br>";
+      echo "This is the last time I'll ask you to listen <br>";
+      echo "I've played all my cards <br>";
+      echo "Would you feel a thing if you saw me right now? <br>";
+      echo "<br>";
+      echo "No matter what I say to you <br>";
+      echo "You're gone (I've been tryin' in vain to hold on) <br>";
+      echo "No matter what I say <br>";
+      echo "<br>";
+      echo "Cause keepin' you close shouldn't be hard<br>";
+      echo "If you were honest when you said you've missed me<br>";
+      echo "You've played with my pride<br>";
+      echo "Making me feel like we had something real <br>";
+      echo "nd making you stay shouldn't feel wrong <br>";
+      echo "This is the last time I'll ask you to listen <br>";
+      echo "I've played all my cards <br>";
+      echo "Would you feel a thing if you saw me right now? <br>";
+      echo "<br>";
+    ?>
+    
+    <button><a href="index.php">Retreat</a></button>
+    
+
+</body>
+
+  <style>
+    body{
+      text-align: center;
+    }
+    button a {
+      text-decoration: none;
+      border: solid 1px darkgray;
+    }
+  </style>
+</html>
