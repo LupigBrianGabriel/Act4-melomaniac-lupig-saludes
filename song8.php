b69fb52869f88b3f340e9b32eb3fe4bbf83d9cda
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:07:16 +0800

    song8 commited by lupig

---
+++ song8.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daisy</title>
+</head>
+<body>
+    <h2>Daisy</h2>
+    <h4>Artist: Wave To Earth</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "Daisy, you're always white in your place <br>";
+      echo "Even between the concrete <br>";
+      echo "Daisy, every time I see you <br>";
+      echo "My day is getting coloured <br>";
+      echo "<br>";
+      echo "Babe, if you can dye me enough <br>";
+      echo "Would my life be different <br>";
+      echo "So, I'll just wait for you to bloom <br>";
+      echo "<br>";
+      echo "Ma planet is like an old black and white TV <br>";
+      echo "Life always goes on and on <br>";
+      echo "But it's never fun at all <br>";
+      echo "Oh, daisy <br>";
+      echo "It's my happiness to run into you <br>";
+      echo "Life always goes on and on <br>";
+      echo "But I'll live with you forever <br>";
+      echo "So, be my daisy <br>";
+      echo "<br>";
+      echo "Oh, sunny day <br>";
+      echo "It's like burn in my youth <br>";
+      echo "Shade is my place but you live by the sun <br>";
+      echo "If you want me to <br>";
+      echo "Well I'll be the rain you need <br>";
+      echo "<br>";
+      echo "Babe, if you can dye me enough <br>";
+      echo "Would my life be different <br>";
+      echo "So, I'll just wait for you to bloom, yeah, yeah, yeah <br>";
+      echo "<br>";
+      echo "Wait for you to bloom <br>";
+      echo "I'll just wait for you to bloom, yeah, yeah <br>";
+      echo "<br>";
+      echo "Ma planet is like an old black and white TV <br>";
+      echo "Life always goes on and on <br>";
+      echo "But it's never fun at all, yeah-ah <br>";
+      echo "But daisy <br>";
+      echo "It's my happiness to run into you <br>";
+      echo "Life always goes on and on <br>";
+      echo "But I'll live with you forever <br>";
+      echo "So, be my daisy <br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
+
