78c06533a0567214e67a1f4d23588df31f497db5
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:06:04 +0800

    song6 commited by lupig

---
+++ song6.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad</title>
+</head>
+<body>
+    <h2>Bad</h2>
+    <h4>Artist: Wave To Earth</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "How could my day be bad<br>";
+      echo "When I'm with you?<br>";
+      echo "You're the only one who makes me laugh<br>";
+      echo "So how can my day be bad?<br>";
+      echo "It's a day for you<br>";
+      echo "<br>";
+      echo "Lately, life's so boring<br>";
+      echo "I've been watching Netflix all day long<br>";
+      echo "I thought there would be<br>";
+      echo "No things left to watch<br>";
+      echo "So I let myself out<br>";
+      echo "<br>";
+      echo "When I went to the park<br>";
+      echo "I recognised you at a glance<br>";
+      echo "Face to face, we just smiled<br>";
+      echo "We already know that we'll be together<br>";
+      echo "<br>";
+      echo "How could my day be bad when I'm with you?<br>";
+      echo "You're the only one who makes me laugh<br>";
+      echo "So how can my day be bad?<br>";
+      echo "It's a day for you<br>";
+      echo "<br>";
+      echo "Coffee in the morning, you and the sun<br>";
+      echo "There's a brown hue in your eyes<br>";
+      echo "How pretty it is<br>";
+      echo "I think I'm in love<br>";
+      echo "<br>";
+      echo "When I went to the park<br>";
+      echo "I recognised you at a glance<br>";
+      echo "Face to face, we smiled<br>";
+      echo "And I finally held your hands<br>";
+      echo "<br>";
+      echo "How could my day be bad when I'm with you?<br>";
+      echo "You're the only one who makes me laugh<br>";
+      echo "So how can my day be bad?<br>";
+      echo "It's a day for you<br>";
+      echo "<br>";
+      echo "How could my day be bad when I'm with you?<br>";
+      echo "You're the only one who gives me love<br>";
+      echo "So how can my day be bad?<br>";
+      echo "It's a day for you<br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
