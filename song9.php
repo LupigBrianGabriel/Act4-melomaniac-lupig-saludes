c37ca5937a6aedf895cbaf355070ed511aaff640
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:07:45 +0800

    song9 commited by lupig

---
+++ song9.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sunny Days</title>
+</head>
+<body>
+    <h2>Sunny Days</h2>
+    <h4>Artist: Wave To Earth</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "Yeah, I've been waiting for this day <br>";
+      echo "The way I'm walking hand in hand with you <br>";
+      echo "Just like all the sunlight is watch over me, baby <br>";
+      echo "Yes, it's full of your smile, yeah <br>";
+      echo "I'll give it to you when I collect even the love in the air, yeah <br>";
+      echo "<br>";
+      echo "The beautiful scenery of the streets <br>";
+      echo "The chirping of birds is nothing without you <br>";
+      echo "I'm nothing without you <br>";
+      echo "Yeah, I'm nothing without you <br>";
+      echo "<br>";
+      echo "We melt this love and recreate it as we imagine it <br>";
+      echo "That's how we make sunny days, yeah <br>";
+      echo "'Cause you're always mysterious to me <br>";
+      echo "<br>";
+      echo "You are my golden age <br>";
+      echo "Everything shines so bright <br>";
+      echo "That my eyes go blind <br>";
+      echo "Our youth never ages here <br>";
+      echo "<br>";
+      echo "We melt this love and recreate it as we imagine it <br>";
+      echo "That's how we make sunny days, yeah <br>";
+      echo "'Cause you're always mysterious to me <br>";
+      echo "<br>";
+      echo "We melt this love and recreate it as we imagine it <br>";
+      echo "That's how we make sunny days, yeah <br>";
+      echo "'Cause you're always mysterious to me <br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
