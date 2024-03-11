7ebed188e7670e1be3e351955b9e205aeeff8b5c
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:04:38 +0800

    song3 commited by lupig

---
+++ song3.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paragraphs</title>
+</head>
+<body>
+    <h2>Paragraphs</h2>
+    <h4>Artist: Luke Chiang</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "How do I tell you I'm still paranoid you'll leave?<br>";
+      echo "How do I tell you that I worry when you say nothing's wrong?<br>";
+      echo "I'm writing paragraphs to find the words to say<br>";
+      echo "Don't wanna upset you, but I needa get this weight off my chest<br>";
+      echo "<br>";
+      echo "Lately you've been in and out of love<br>";
+      echo "And I've been feeling in and out of touch<br>";
+      echo "<br>";
+      echo "I'll keep your flowers<br>";
+      echo "While I'm still waitin', playing<br>";
+      echo "Broken guitars in my room<br>";
+      echo "Maybe I don't think you ever have to leave, oh no<br>";
+      echo "<br>";
+      echo "Maybe the way you feel is out of my control<br>";
+      echo "And if I'm honest, I know having you's too good to be true<br>";
+      echo "But I can't help myself from giving you my all<br>";
+      echo "Not gonna stand and watch this thing we have get torn up in two<br>";
+      echo "<br>";
+      echo "Lately you've been in and out of love<br>";
+      echo "And I've been feeling in and out of touch<br>";
+      echo "<br>";
+      echo "So I'll keep your flowers<br>";
+      echo "While I'm still waitin, playing<br>";
+      echo "Broken guitars<br>";
+      echo "I only wish you'd call me back it's been hours<br>";
+      echo "I don't think you have to leave, oh no<br>";
+      echo "<br>";
+      echo "Even if you run away (So I'll keep your flowers)<br>";
+      echo "Would you be my, runaway (While I'm still waitin')<br>";
+      echo "Cause I'll still be standing here (Playin broken guitars)<br>";
+      echo "For God knows how many days (I only wish you'd call me back)<br>";
+      echo "Even if you run away (It's been hours)<br>";
+      echo "Would you be my, runaway (I don't think you have to leave)<br>";
+      echo "Cause I'll still be standing here (Oh no)<br>";
+      echo "For God knows how many days<br>";
+      echo "<br>";
+      echo "So how do I tell you I'm still paranoid you'll leave<br>";
+      echo "How do I tell you that I worry when you say nothing's wrong<br>";
+      echo "I'm writing paragraphs to find the words to say<br>";
+      echo "Don't wanna upset you, but I needa get this weight off my chest<br>";
+      echo "<br>";
+      echo "So I'll keep your flowers<br>";
+      echo "While I'm still waitin, playing<br>";
+      echo "Broken guitars in my room<br>";
+      echo "Maybe I don't think you ever have to leave<br>";
+      echo "<br>";
+      echo "Maybe I don't think you ever have to leave<br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
