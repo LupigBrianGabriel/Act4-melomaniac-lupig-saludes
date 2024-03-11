12f206970e59412ae988515e1b0ef8381393ced3
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:05:08 +0800

    song4 commited by lupig

---
+++ song4.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Used To Me</title>
+</head>
+<body>
+    <h2>Used To Me</h2>
+    <h4>Artist: Luke Chiang</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "Don't stay up too late <br>";
+      echo "But I gotta let you know that you've been on my mind <br>";
+      echo "Taking a while for me to say it, hope it's fine <br>";
+      echo "And I wouldn't mind if you were taking up my time <br>";
+      echo "So I'll meet you at 8 <br>";
+      echo "Whatever you wanna do it's fine by me I swear <br>";
+      echo "Put on the radio and take you anywhere <br>";
+      echo "'Cause I've been tired of only talking when we text <br>";
+      echo "I hope it's mutual cause I think I could get used to this <br>";
+      echo "Wondering If you think you'd get used to me <br>";
+      echo "Now I'm getting nervous like I used to be <br><br>";
+      echo "<br>";
+      echo "Asking you to say the word and I'll be on the way <br>";
+      echo "(Let's just go talk about it, I got time if you're about it) <br>";
+      echo "If you need me I'll be here to stay <br>";
+      echo "(Let's just go talk about it, I got time just) <br><br>";
+      echo "<br>";
+      echo "Don't stay up too late <br>";
+      echo "But If you find yourself awake at 2AM <br>";
+      echo "You can call me up it doesn't matter when <br>";
+      echo "Not gonna lie to you I'll be here anytime <br>";
+      echo "So let's not complicate <br>";
+      echo "Whatever emotions that we're feeling at this time <br>";
+      echo "When we got issues we can solve 'em down the line <br>";
+      echo "And if you feel the same I hope you won't get tired of me <br>";
+      echo "Do my best to offer something new to you <br>";
+      echo "You don't need to doubt that I'll be true to you <br><br>";
+      echo "<br>";
+      echo "I promise If you say the word and I'll be on the way <br>";
+      echo "(Let's just go talk about it, I got time if you're about it) <br>";
+      echo "If you need me I'll be here to stay <br>";
+      echo "(Let's just go talk about it, I got time if you're about it) <br>";
+      echo "Say the word and I'll be on the way <br>";
+      echo "(Let's just go talk about it, I got time if you're about it) <br>";
+      echo "If you need me I'll be here to stay <br>";
+      echo "(Let's just go talk about it, I got time if you're about it) <br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
