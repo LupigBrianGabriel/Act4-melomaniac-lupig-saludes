517c605efeda8a5dde75acb72ed971bca857f89b
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:03:32 +0800

    song2 commited by lupig

---
+++ song2.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>May I Ask</title>
+</head>
+<body>
+    <h2>May I Ask</h2>
+    <h4>Artist: Luke Chiang</h4>
+
+    <?php
+      echo"Lyrics: <br><br>";
+      echo "May I ask you how you've been <br>";
+      echo "Would I have the words to say <br>";
+      echo "I've forgotten how to feel <br>";
+      echo "How to call you by your name <br>";
+      echo "<br>";
+      echo "Barely recognize you now <br>";
+      echo "All your words have turned to stone <br>";
+      echo "You've forgotten all my songs <br>";
+      echo "You've forgotten all my love <br>";
+      echo "<br>";
+      echo "Don't you know, you make me nervous still <br>";
+      echo "You pull my thoughts apart <br>";
+      echo "Though I've been tryna get you off my mind <br>";
+      echo "Give me time cause it still burns my lips <br>";
+      echo "To say your name out loud, my love <br>";
+      echo "<br>";
+      echo "And by now, your smile has changed <br>";
+      echo "You don't look at me the same <br>";
+      echo "It's naive of me to think <br>";
+      echo "That one day you'll miss my name <br>";
+      echo "<br>";
+      echo "Promises you said you'd keep <br>";
+      echo "Empty lines between the scenes <br>";
+      echo "Was it wrong for me to trust? <br>";
+      echo "I'm still only 17 <br>";
+      echo "<br>";
+      echo "Don't you know, you make me nervous still <br>";
+      echo "You pull my thoughts apart <br>";
+      echo "Though I've been tryna get you off my mind <br>";
+      echo "Give me time cause it still burns my lips <br>";
+      echo "To say your name out loud, my love <br>";
+      echo "<br>";
+      echo "It's just my natural reaction to you <br>";
+      echo "Making me feel like I lost sanity <br>";
+      echo "And I can't help but to miss you <br>";
+      echo "Though I know there's never a chance <br>";
+      echo "<br>";
+      echo "Don't you know, you make me nervous still <br>";
+      echo "You pull my thoughts apart <br>";
+      echo "Though I've been tryna get you off my mind <br>";
+      echo "Give me time cause it still burns my lips <br>";
+      echo "To say your name out loud, my love <br>";
+      echo "<br>";
+      ?>
+    
+      <button><a href="index.php">Retreat</a></button>
+      
+  
+  <body>
+  
+    <style>
+      body{
+        text-align: center;
+      }
+      button a {
+        text-decoration: none;
+        border: solid 1px darkgray;
+      }
+    </style>
+  </html>
