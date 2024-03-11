1ffe7f5be12d889074bf2c0220d83244f24c09e0
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-03-11 21:01:28 +0800

    index commited by lupig

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h1>My Song Book</h1>
+    <ol>
+        <li><a href="song1.php">Shouldn't Be</a></li>
+        <li><a href="song2.php">May I Ask</a></li>
+        <li><a href="song3.php">Paragraphs</a></li>
+        <li><a href="song4.php">Used To Me</a></li>
+        <li><a href="song5.php">Home</a></li>
+        <li><a href="song6.php">Bad</a></li>
+        <li><a href="song7.php">Seasons</a></li>
+        <li><a href="song8.php">Daisy</a></li>
+        <li><a href="song9.php">Sunny Days</a></li>
+    </ol>
+</body>
+</html>
